db8ae7d0bea801ddb53fee433d447877e9bee370
AmitaiB <blickstein@gmail.com>
2017-09-20 12:06:05 -0400

    Adds Optional+Extensions

---
+++ MyPhotoViewer/Supporting/Optional+Extensions.swift
@@ -0,0 +1,51 @@
+//
+//  Optional+Extensions.swift
+//  MyPhotoViewer
+//
+//  from: OptionalExtensions
+//
+//  Created by Rui Peres on 30/12/2015.
+//  Copyright © 2015 Rui Peres. All rights reserved.
+//
+
+public extension Optional {
+	
+	func filter(_ predicate: (Wrapped) -> Bool) -> Optional {
+		return map(predicate) == .some(true) ? self : .none
+	}
+	
+	func mapNil(_ predicate: () -> Wrapped) -> Optional {
+		return self ?? .some(predicate())
+	}
+	
+	func flatMapNil(_ predicate: () -> Optional) -> Optional {
+		return self ?? predicate()
+	}
+	
+	func then(_ f: (Wrapped) -> Void) {
+		if let wrapped = self { f(wrapped) }
+	}
+	
+	func maybe<U>(_ defaultValue: U, f: (Wrapped) -> U) -> U {
+		return map(f) ?? defaultValue
+	}
+	
+	func onSome(_ f: (Wrapped) -> Void) -> Optional {
+		then(f)
+		return self
+	}
+	
+	func onNone(_ f: () -> Void) -> Optional {
+		if isNone { f() }
+		return self
+	}
+	
+	var isSome: Bool {
+		return self != nil
+	}
+	
+	var isNone: Bool {
+		return !isSome
+	}
+}
+
