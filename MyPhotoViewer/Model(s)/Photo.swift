9d06811f248580a9d05ac965b80e26ff08409276
AmitaiB <blickstein@gmail.com>
2017-09-20 12:05:02 -0400

    Adds PhotoData model

---
+++ MyPhotoViewer/Model(s)/Photo.swift
@@ -0,0 +1,18 @@
+//
+//  Photo.swift
+//  MyPhotoViewer
+//
+//  Created by Amitai Blickstein on 9/20/17.
+//  Copyright © 2017 Amitai Blickstein. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoData: Decodable {
+	let albumId: Int
+	let id: Int
+	let title: String
+	let url: URL
+	let thumbnailUrl: URL
+}
+
