70c3f0baf8c5ee67e7cf2bdcefab951f4dba3845
AmitaiB <blickstein@gmail.com>
2017-09-20 12:05:40 -0400

    Adds Errors

---
+++ MyPhotoViewer/Supporting/Errors.swift
@@ -0,0 +1,14 @@
+//
+//  Errors.swift
+//  MyPhotoViewer
+//
+//  Created by Amitai Blickstein on 9/20/17.
+//  Copyright © 2017 Amitai Blickstein. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+	case responseFailed(reason: String)
+}
+
